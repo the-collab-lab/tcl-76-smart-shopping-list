0574c45c1c172196a869e9378b64d1af9c732e5d
mindyzwan <mindyzwan@gmail.com>
2024-08-11 08:06:16 -0700

    Remove pairing work

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 
 import './Layout.css';
 import { auth } from '../api/config.js';
-import { useAuth, SignInButton, SignOutButton } from '../api/useAuth.jsx';
 
 /**
  * TODO: The links defined in this file don't work!
@@ -13,20 +12,11 @@ import { useAuth, SignInButton, SignOutButton } from '../api/useAuth.jsx';
  */
 
 export function Layout() {
-	const { user } = useAuth();
 	return (
 		<>
 			<div className="Layout">
 				<header className="Layout-header">
 					<h1>Smart shopping list</h1>
-					{!!user ? (
-						<div>
-							<span>Signed in as {auth.currentUser.displayName}</span> (
-							<SignOutButton />)
-						</div>
-					) : (
-						<SignInButton />
-					)}
 				</header>
 				<main className="Layout-main">
 					<Outlet />
