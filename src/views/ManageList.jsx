0d4d3a91cb3df20ec0767f87c7cc681a6cb53af6
arandel1 <allison.randel@gmail.com>
2024-08-19 12:43:29 -0500

    beginning form, added labels, added input for the name of item and a dropdown selection for user to select when they will need the item again

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+
 export function ManageList() {
+	// const [formData, setFormData] = useState(false);
+
 	return (
-		<p>
-			Hello from the <code>/manage-list</code> page!
-		</p>
+		<>
+			<p>
+				Hello from the <code>/manage-list</code> page!
+			</p>
+
+			<div className="manage-list-form">
+				<form>
+					{/* // name of item
+						// days until next purchase: soon, kind of soon, not soon
+						// label feature */}
+
+					<label htmlFor="name">Name of item</label>
+					<input type="text" name="name-of-item" id="name-of-item"></input>
+
+					<label htmlFor="frequency">
+						When will you need this item again?:
+					</label>
+
+					<select id="frequency">
+						<option value="soon">Soon (7 days)</option>
+						<option value="kind-of-soon">Kind of Soon (14 Days)</option>
+						<option value="not-soon">Not Soon (30 Days)</option>
+					</select>
+				</form>
+			</div>
+		</>
 	);
 }
