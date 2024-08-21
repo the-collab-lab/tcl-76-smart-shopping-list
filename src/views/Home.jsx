4d51886892165d5347afbaf89218b08f5152f5c5
Sarah Meko <tmekos1@gmail.com>
2024-08-21 09:46:25 -0700

    create new list form and handle submit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,36 @@
 import './Home.css';
 import { SingleList } from '../components';
+import { useState } from 'react';
+import { createList, useAuth } from '../api';
+// import { useNavigate } from "react-router-dom";
+// import { useStateWithStorage } from './utils';
+
 export function Home({ data, setListPath }) {
+	const [listName, setListName] = useState('');
+	const [error, setError] = useState('');
+	const { user } = useAuth();
+	const userId = user?.uid;
+	const userEmail = user?.email;
+	// const navigate = useNavigate();
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		try {
+			await createList(userId, userEmail, listName);
+			// console.log(createList(userId, userEmail, listName))
+			//setListPath(pathName)
+			/* need to pass in the new list path into setListPath, and then it should save to localStorage */
+			//save to local storage
+			setListName('');
+			setError('List saved to database');
+			// navigate("/list")
+		} catch (error) {
+			setListName('')(setError('List Not saved to database'));
+			console.log(error);
+		}
+	}
+
 	return (
 		<div className="Home">
 			<p>
@@ -10,15 +40,28 @@ export function Home({ data, setListPath }) {
 				{data &&
 					data.map((list) => {
 						return (
-							<SingleList
-								key={crypto.randomUUID()}
-								name={list.name}
-								setListPath={setListPath}
-								path={list.path}
-							/>
+							<>
+								<SingleList
+									key={crypto.randomUUID()}
+									name={list.name}
+									setListPath={setListPath}
+									path={list.path}
+								/>
+							</>
 						);
 					})}
 			</ul>
+			<form action="" onSubmit={handleSubmit}>
+				<label htmlFor="listName">Enter List Name:</label>
+				<input
+					type="text"
+					id="listName"
+					value={listName}
+					onChange={(e) => setListName(e.target.value)}
+				/>
+				<button>Submit</button>
+				<p>{error}</p>
+			</form>
 		</div>
 	);
 }
