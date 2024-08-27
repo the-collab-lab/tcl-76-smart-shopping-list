cdc9ff48cc52205cba1c30d02d2e336acaecf514
arandel1 <allison.randel@gmail.com>
2024-08-27 14:50:04 -0500

    input renders and clear button functions properly

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,17 +1,71 @@
 import { ListItem } from '../components';
+import { useState, useEffect } from 'react';
 
 export function List({ data }) {
+	const [filterVal, setFilterVal] = useState('');
+	let filteredItems = data;
+
+	const filter = filteredItems.filter((x) => x.name.startsWith(filterVal));
+
+	function renderList() {
+		for (let item of filteredItems) {
+			return <p>{item.name}</p>;
+		}
+	}
+
+	useEffect(() => {
+		//  console.log(data.map((item) => {
+		// 	Object.item.name
+		//  }))
+
+		for (let item of data) {
+			if (item.name.startsWith(filterVal)) {
+				filteredItems.push(item);
+				// console.log(item.name)
+			}
+		}
+
+		// let filteredData = data.filter(item => item.name.includes(filterVal) )
+
+		// console.log(filteredItems)
+	}, [filterVal]);
+
+	const clearInput = (e) => {
+		e.preventDefault();
+		setFilterVal('');
+	};
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+
+			<form onSubmit={clearInput}>
+				<label htmlFor="item-name"> Item name:</label>
+				<input
+					id="item-name"
+					name="item-name"
+					type="text"
+					value={filterVal}
+					onChange={(e) => setFilterVal(e.target.value)}
+				/>
+				<button>Clear</button>
+			</form>
+
 			<ul>
 				{data &&
 					data.map((list) => {
 						return <ListItem key={list.id} name={list.name} />;
 					})}
 			</ul>
+
+			{/* <ul>
+				{filter &&
+					filter.map((list) => {
+						return <ListItem key={list.id} name={list.name} />;
+					})}
+			</ul> */}
 		</>
 	);
 }
