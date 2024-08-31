a7992403ae0ca05fdf26e6323747635100a8bb2a
emmacodes <binma.emma@gmail.com>
2024-08-30 22:26:23 -0700

    added invite friend form and connect with shareList func

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -139,14 +139,14 @@ export async function createList(userId, userEmail, listName) {
 export async function shareList(listPath, currentUserId, recipientEmail) {
 	// Check if current user is owner.
 	if (!listPath.includes(currentUserId)) {
-		return;
+		return { response: "You don't have access to this shopping list." };
 	}
 	// Get the document for the recipient user.
 	const usersCollectionRef = collection(db, 'users');
 	const recipientDoc = await getDoc(doc(usersCollectionRef, recipientEmail));
 	// If the recipient user doesn't exist, we can't share the list.
 	if (!recipientDoc.exists()) {
-		return;
+		return { response: 'User does not existed' };
 	}
 	// Add the list to the recipient user's sharedLists array.
 	const listDocumentRef = doc(db, listPath);
@@ -154,6 +154,8 @@ export async function shareList(listPath, currentUserId, recipientEmail) {
 	updateDoc(userDocumentRef, {
 		sharedLists: arrayUnion(listDocumentRef),
 	});
+
+	return { response: 'The shopping list has been shared!' };
 }
 
 /**
