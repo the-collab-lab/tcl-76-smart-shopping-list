e143256d9db43d32358687a04fcb94bf93714633
MarcosPerez16 <marcos.perez06@yahoo.com>
2024-09-01 00:50:31 -0700

    fixed typo in firebase.js file in shareList function.

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -146,7 +146,7 @@ export async function shareList(listPath, currentUserId, recipientEmail) {
 	const recipientDoc = await getDoc(doc(usersCollectionRef, recipientEmail));
 	// If the recipient user doesn't exist, we can't share the list.
 	if (!recipientDoc.exists()) {
-		return { response: 'User does not existed' };
+		return { response: 'User does not exist' };
 	}
 	// Add the list to the recipient user's sharedLists array.
 	const listDocumentRef = doc(db, listPath);
