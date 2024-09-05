4ac21d8d28213c9f3f59b3a6b8f6f98d8ec36a57
emmacodes <binma.emma@gmail.com>
2024-09-05 11:25:43 -0700

    added dark mode theme to Modal component

--- src/views/Modal.jsx
+++ src/views/Modal.jsx
@@ -4,24 +4,27 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { useNavigate } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
 
-const style = {
+const style = (theme) => ({
 	position: 'absolute',
 	top: '50%',
 	left: '50%',
 	transform: 'translate(-50%, -50%)',
 	width: 400,
-	bgcolor: 'background.paper',
-	border: '2px solid #000',
+	bgcolor: theme.palette.mode === 'dark' ? '#424242' : 'background.paper',
+	color: theme.palette.text.primary,
 	boxShadow: 24,
 	p: 4,
 	fontSize: 20,
-};
+});
 
-export default function BasicModal(dataEmpty) {
+export default function BasicModal({ dataEmpty }) {
 	const [open, setOpen] = React.useState(dataEmpty);
 	const handleClose = () => setOpen(false);
 	const navigate = useNavigate();
+	const theme = useTheme();
+
 	return (
 		<div>
 			<Modal
@@ -30,7 +33,7 @@ export default function BasicModal(dataEmpty) {
 				aria-labelledby="modal-modal-title"
 				aria-describedby="modal-modal-description"
 			>
-				<Box sx={style}>
+				<Box sx={style(theme)}>
 					<Typography id="modal-modal-title" variant="h4" component="h2">
 						Tip:
 					</Typography>
@@ -41,11 +44,12 @@ export default function BasicModal(dataEmpty) {
 						sx={{
 							borderRadius: '8px',
 							fontSize: '12px',
+							color: theme.palette.primary.main,
 						}}
 						size="large"
 						onClick={() => navigate('/manage-list')}
 					>
-						Go to Manage LIst
+						Go to Manage List
 					</Button>
 				</Box>
 			</Modal>
