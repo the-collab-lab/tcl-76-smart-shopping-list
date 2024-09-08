b7193bde65c167aef5f3c3c5cd5a6a6f914f6e75
emmacodes <binma.emma@gmail.com>
2024-09-07 22:45:16 -0700

    delay the prompt message

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -5,6 +5,17 @@ import BasicModal from './Modal';
 export function List({ data, userId }) {
 	const [filterVal, setFilterVal] = useState('');
 	const [filteredList, setFilteredList] = useState([]);
+	const [showModal, setShowModal] = useState(false);
+
+	const dataEmpty = userId && !data.length;
+
+	useEffect(() => {
+		if (dataEmpty) {
+			setTimeout(() => {
+				setShowModal(true);
+			}, 2000);
+		}
+	}, []);
 
 	const clearInput = (e) => {
 		e.preventDefault();
@@ -24,8 +35,7 @@ export function List({ data, userId }) {
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
-
-			{userId && !data.length && <BasicModal dataEmpty={true} />}
+			{showModal && dataEmpty && <BasicModal dataEmpty={dataEmpty} />}
 
 			<form onSubmit={clearInput}>
 				<label htmlFor="item-name"> Item name:</label>
