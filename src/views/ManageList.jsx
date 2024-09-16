48c2f5174b13057b3aa501dca4bf0aa8d4650bf6
Sarah Meko <tmekos1@gmail.com>
2024-09-16 10:15:14 -0700

    removed third includes check

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -30,7 +30,7 @@ export function ManageList({ userId, data }) {
 		const listPath = localStorage.getItem('tcl-shopping-list-path');
 
 		if (!listPath) {
-			window.alert('List is not existed.');
+			window.alert('List does not exist.');
 			return;
 		}
 
@@ -38,37 +38,26 @@ export function ManageList({ userId, data }) {
 			.replace(/[^A-Z0-9]/gi, '')
 			.toLowerCase();
 
-		// if "exact" match found, alert user exit handleSubmit early, don't add to database/ dont edit similar item and its timestamp
-		// checks if form data is a match without whitespace and regardless of case
 		if (
 			data.find(
 				(item) =>
 					item.name.trim().toLowerCase() === formData.name.trim().toLowerCase(),
 			)
 		) {
-			window.alert(`${formData.name} already exists in your list`);
+			window.alert(`${formData.name} already exists in your list.`);
 			return;
 		}
 
-		// check for not-so-exact matches - gives users a choice
 		for (const item of data) {
-			// use regex to find mutliple edge cases for characters and spaces throughout string
 			const itemCheck = item.name.replace(/[^A-Z0-9]/gi, '').toLowerCase();
-			if (
-				itemCheck === formDataCheck ||
-				formDataCheck.includes(itemCheck) ||
-				itemCheck.includes(formDataCheck)
-			) {
-				// if detected give user option to add if theyd like
+			if (itemCheck === formDataCheck || formDataCheck.includes(itemCheck)) {
 				if (
 					!window.confirm(
 						`A similar item is already on your list. Do you still want to add ${formData.name}?`,
 					)
 				) {
-					// if they don't confirm exit out of handlesubmit - item not saved
 					return;
 				} else {
-					// leave loop and add item
 					break;
 				}
 			}
