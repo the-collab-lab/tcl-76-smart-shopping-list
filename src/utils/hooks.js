c42e43bbd20024bd8924c05c16a8953f66501d69
emmacodes <binma.emma@gmail.com>
2024-09-30 10:59:55 -0700

    added a detectBrowser function, if safari, disable voiceToText feature with an alert msg

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -61,11 +61,39 @@ export function useVoiceToText() {
 		};
 	}, []);
 
+	function detectBrowser() {
+		let userAgent = navigator.userAgent;
+		if (userAgent.indexOf('Edg') > -1) {
+			return 'Microsoft Edge';
+		} else if (userAgent.indexOf('Chrome') > -1) {
+			return 'Chrome';
+		} else if (userAgent.indexOf('Firefox') > -1) {
+			return 'Firefox';
+		} else if (userAgent.indexOf('Safari') > -1) {
+			return 'Safari';
+		} else if (userAgent.indexOf('Opera') > -1) {
+			return 'Opera';
+		} else if (
+			userAgent.indexOf('Trident') > -1 ||
+			userAgent.indexOf('MSIE') > -1
+		) {
+			return 'Internet Explorer';
+		}
+
+		return 'Unknown';
+	}
+
 	const startListening = async () => {
 		try {
 			// Request microphone access
 			await navigator.mediaDevices.getUserMedia({ audio: true });
 			console.log('Microphone access granted. Starting speech recognition...');
+
+			const browserName = detectBrowser();
+			if (browserName === 'Safari') {
+				window.alert('Start Voice Input is not working in your browser.');
+				return;
+			}
 
 			if (!isListening && recognitionRef.current) {
 				recognitionRef.current.start();
