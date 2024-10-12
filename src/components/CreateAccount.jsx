4e95ceab32c3634c7f922533fc8da028cf793ae3
MarcosPerez16 <marcos.perez06@yahoo.com>
2024-10-11 20:05:57 -0700

    installed firebase authentication, refactored `handleSubmit` function in CreateAccount.jsx to utillize `addUserToDatabase` function for account creation. Added name input for account creation.

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -1,19 +1,54 @@
 import { useState } from 'react';
+import { auth } from '../api/config';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { addUserToDatabase } from '../api';
 
 export function CreateAccount() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [name, setName] = useState('');
+	const [error, setError] = useState('');
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		//logic for handling form submission when creating account
-		console.log('Creating account with email:', email);
+
+		try {
+			//create user with email and password
+			const userCredential = await createUserWithEmailAndPassword(
+				auth,
+				email,
+				password,
+			);
+			const user = userCredential.user;
+
+			//Add the user to firestore
+			await addUserToDatabase({ ...user, displayName: name });
+
+			//Reset input fields or redirect as needed
+			setEmail('');
+			setPassword('');
+			setName('');
+			console.log('User registered successfully');
+		} catch (error) {
+			//Handle errors
+			console.error('Error signing up', error);
+			setError(error.message);
+		}
 	};
 
 	return (
 		<div>
 			<h2>Create Account</h2>
 			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="name">Name:</label>
+					<input
+						type="text"
+						id="name"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
 				<div>
 					<label htmlFor="email">Email:</label>
 					<input
@@ -31,6 +66,7 @@ export function CreateAccount() {
 					/>
 				</div>
 				<button type="submit">Create Account</button>
+				{error && <p>{error}</p>}
 			</form>
 		</div>
 	);
